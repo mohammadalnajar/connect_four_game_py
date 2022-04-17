fc5106fb80d8651cbe8d116133cc066fd4a5c5ed
Mohammad-Al-Najar <44784702+mohammadalnajar@users.noreply.github.com>
2022-04-17 16:53:57 +0200

    create random circle func

---
+++ script.py
@@ -0,0 +1,16 @@
+import random
+
+
+def create_dict():
+    ran_letter_idx = random.randint(0, 6)
+    ran_number_idx = random.randint(0, 5)
+    dict = {}
+    letters_list = ["a", "b", "c", "d", "e", "f", "g"]
+    numbers_list = [1, 2, 3, 4, 5, 6]
+    dict[letters_list[ran_letter_idx]] = numbers_list[ran_number_idx]
+    return dict
+
+
+test_dict = create_dict()
+
+print(test_dict)
