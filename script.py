cafbc33f1fd30756a3a107e837a3e38470c49409
Mohammad-Al-Najar <44784702+mohammadalnajar@users.noreply.github.com>
2022-04-17 17:38:17 +0200

    add circle class

--- script.py
+++ script.py
@@ -1,5 +1,11 @@
 import random
 
+green_color = "\x1b[6;30;42m"
+red_color = "\x1b[6;30;41m"
+yellow_color = "\x1b[6;30;43m"
+blue_color = "\x1b[6;30;44m"
+reset_color = "\x1b[0m"  # reset color
+
 
 def create_dict():
     ran_letter_idx = random.randint(0, 6)
@@ -14,3 +20,18 @@ def create_dict():
 test_dict = create_dict()
 
 print(test_dict)
+
+
+class Circle:
+    def __init__(self, letter, number, row, col) -> None:
+        self.letter = letter
+        self.number = number
+        self.row = row
+        self.col = col
+        self.owner = None
+
+    def __repr__(self) -> str:
+        return f"{self.letter}{self.number}"
+
+    def set_owner(self, player):
+        self.owner = player
