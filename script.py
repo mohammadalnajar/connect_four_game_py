b4b78ea971a8bd55b32679fe31184fac1f9d6b4b
Mohammad-Al-Najar <44784702+mohammadalnajar@users.noreply.github.com>
2022-04-17 19:49:19 +0200

    create class player

--- script.py
+++ script.py
@@ -41,6 +41,50 @@ class Circle:
             return False  # failed to set owner
 
 
+class Player:
+    count = 1  # count of players
+
+    def __init__(self) -> None:
+        name = input(f"Enter a name for player {Player.count}: ")
+
+        # check if the name is valid
+        while True:
+            try:
+                if not type(name) is str or name.isnumeric():
+                    raise ValueError
+                break
+            except ValueError:
+                print("Please enter a valid name(only text is allowed)")
+                name = input(f"Enter a name for player {Player.count}: ")
+
+        self.name = name
+
+        color = input(
+            f"Enter a color for player {Player.count} ('g' for green / 'r' for red ): "
+        )
+        # check if the color is valid
+        while True:
+            try:
+                if color.isnumeric() or color.lower() not in ["g", "r", "red", "green"]:
+                    raise ValueError
+                break
+            except ValueError:
+                print("Please enter a valid color(only 'g' or 'r' is allowed)")
+                color = input(
+                    f"Enter a color for player {Player.count} ('g' for green / 'r' for red ): "
+                )
+        self.color = green_color if color == "g" or color == "green" else red_color
+
+        self.color_name = "green" if color == "g" or color == "green" else "red"
+
+        self.circles = []
+
+        Player.count += 1
+
+    def __repr__(self) -> str:
+        return f"The player {self.name} has the color {self.color} {self.color_name} {reset_color}"
+
+
 def fill_in_circles():
     for row in range(1, 7):  # for each row
         for col in range(1, 8):  # for each column
@@ -77,6 +121,11 @@ def print_circles():
 
 print("=" * 60)
 print_circles()
+print("=" * 60)
+
+player_one = Player()
+
+print(player_one)
 
 print(yellow_color + "Warning!" + reset_color)
 print(red_color + "Failure!" + reset_color)
